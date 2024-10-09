dbe5f77e1249c87ef0d6949d3000afa18802dd32
PedroHenriqueCustodiodeLima <ph4261009@gmail.com>
2024-10-09 08:18:49 -0300

    index ajustado

--- index.js
+++ index.js
@@ -1,152 +1,317 @@
-const rowsPerPage = 15; 
+const rowsPerPage = 20; 
 const pagesPerSet = 10; 
 let currentPage = 1;
-let currentSet = 1; 
-const tableRows = document.querySelectorAll('#table-body tr');
-const totalPages = Math.ceil(tableRows.length / rowsPerPage);
+let currentSet = 1;
+const tableBody = document.getElementById('table-body');
+let tableRows = Array.from(tableBody.querySelectorAll('tr')); 
+let filteredRows = tableRows; 
 const paginationContainer = document.getElementById('pagination-container');
 const prevSetBtn = document.getElementById('prev-set');
 const nextSetBtn = document.getElementById('next-set');
 const pageNumbersContainer = document.getElementById('page-numbers');
-      
-function showPage(page) {
+
+const intervalTime = 10000; 
+let autoPageInterval; 
+let progressInterval; 
+
+function showPage(page, rows) {
     const start = (page - 1) * rowsPerPage;
-    const end = page * rowsPerPage;
-    tableRows.forEach((row, index) => {
-        row.style.display = 'none';
-            if (index >= start && index < end) {
-                row.style.display = '';
-        }
-    });
-}
-function updatePageNumbers() {
-    pageNumbersContainer.innerHTML = '';
-    const totalVisiblePages = Math.min(pagesPerSet, totalPages - ((currentSet - 1) * pagesPerSet));
-    const startPage = (currentSet - 1) * pagesPerSet + 1;
-    for (let i = 0; i < totalVisiblePages; i++) {
-        const pageNumber = startPage + i;
-        if (pageNumber <= totalPages) {
-            const pageButton = document.createElement('button');
-            pageButton.innerText = pageNumber;
-            pageButton.classList.add('btn', 'btn-light', 'mx-1');
-            pageButton.onclick = () => {
-                currentPage = pageNumber;
-                showPage(currentPage);
-            };
-            pageNumbersContainer.appendChild(pageButton);
-        }
+    const end = start + rowsPerPage; 
+
+    tableBody.innerHTML = ''; 
+    const rowsToDisplay = rows.slice(start, end); 
+    if (rowsToDisplay.length === 0) {
+        tableBody.innerHTML = '<tr><td colspan="X">Nenhum dado para exibir</td></tr>'; 
+    } else {
+        rowsToDisplay.forEach(row => tableBody.appendChild(row));
     }
 }
 
-function updateNavigationButtons() {
+function updatePagination(rows) {
+    const totalPages = Math.ceil(rows.length / rowsPerPage);
+    const totalSets = Math.ceil(totalPages / pagesPerSet);
+    const startSet = (currentSet - 1) * pagesPerSet + 1;
+    const endSet = Math.min(startSet + pagesPerSet - 1, totalPages);
+    
+    pageNumbersContainer.innerHTML = '';
+
+    for (let i = startSet; i <= endSet; i++) {
+        const pageNumber = document.createElement('button');
+        pageNumber.className = 'btn btn-success mx-1';
+        pageNumber.textContent = i;
+        
+        if (i === currentPage) {
+            pageNumber.classList.add('active-page');
+        }
+
+        pageNumber.onclick = () => {
+            currentPage = i;
+            clearInterval(autoPageInterval); 
+            clearInterval(progressInterval); 
+            updateTableAndPagination(filteredRows);
+            updateProgressBar(); 
+        };
+
+        pageNumbersContainer.appendChild(pageNumber);
+    }
+
     prevSetBtn.disabled = currentSet === 1;
-    nextSetBtn.disabled = currentSet * pagesPerSet >= totalPages;
+    nextSetBtn.disabled = currentSet === totalSets;
+
+    showPage(currentPage, filteredRows);
 }
 
-prevSetBtn.onclick = () => {
-    currentSet--;
-    updatePageNumbers();
-    showPage(currentPage);
-    updateNavigationButtons();
-};
+function updateTableAndPagination(rows) {
+    showPage(currentPage, rows); 
+    updatePagination(rows); 
+}
 
-nextSetBtn.onclick = () => {
-    currentSet++;
-    updatePageNumbers();
-    showPage(currentPage);
-    updateNavigationButtons();
-};
+function nextPage() {
+    const totalPages = Math.ceil(filteredRows.length / rowsPerPage);
+    console.log(`Total de páginas: ${totalPages}, Página atual antes: ${currentPage}`);
+    currentPage++;
+    if (currentPage > totalPages) {
+        currentPage = 1; 
+    }
+    
+    console.log(`Página atual após: ${currentPage}`);
+
+
+    updateTableAndPagination(filteredRows);
+    updateProgressBar(); 
+}
+
+function startAutoPagination() {
+    updateProgressBar(); 
+    autoPageInterval = setInterval(nextPage, intervalTime); 
+}
+
+function updateProgressBar() {
+    const progressBar = document.getElementById('progress-bar');
+    progressBar.style.width = '0%'; 
+    let startTime = null; 
+    const duration = intervalTime; 
+
+    function animateProgress(timestamp) {
+        if (!startTime) startTime = timestamp; 
+        const elapsed = timestamp - startTime; 
+        const progress = Math.min((elapsed / duration) * 100, 100); 
+
+        progressBar.style.width = progress + '%'; 
+
+        if (progress < 100) {
+            requestAnimationFrame(animateProgress); 
+        }
+    }
+
+    requestAnimationFrame(animateProgress); 
+}
+
+function stopAutoPagination() {
+    clearInterval(autoPageInterval);
+    clearInterval(progressInterval);
+}
+showPage(currentPage, filteredRows);
+startAutoPagination(); 
+
+
+
 
 function filterTable() {
-    const filterInput = document.getElementById('filterInput');
-    const filterValue = filterInput.value.toLowerCase();
-    let filteredCount = 0;
-    const tableBody = document.getElementById('table-body');
-    tableBody.innerHTML = '';
-    const filteredRows = [];
-    tableRows.forEach(row => {
-        let matchFound = false; 
-        for (let cell of row.cells) {
-            if (cell.textContent.toLowerCase().includes(filterValue)) {
-                matchFound = true;
-                break; 
-            }
-        }
-        if (matchFound) {
-            const newRow = row.cloneNode(true);
-            filteredRows.push(newRow);
-            filteredCount++;
-        }
-    });
-    if (filteredCount > 0) {
-        filteredRows.forEach(filteredRow => tableBody.appendChild(filteredRow));
-    } else {
-        const noResultsRow = document.createElement('tr');
-        noResultsRow.innerHTML = '<td colspan="9" style="text-align:center;">Nenhum resultado encontrado.</td>';
-        tableBody.appendChild(noResultsRow);
-    }
-    currentPage = 1; 
-    updatePageNumbers();
-    showPage(currentPage);
-    updateNavigationButtons();
-}
-
-showPage(currentPage);
-updatePageNumbers();
-updateNavigationButtons();
-window.onscroll = function() {
-    const button = document.getElementById('scrollToTop');
-    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-        button.style.display = "block";
-    } else {
-        button.style.display = "none";
-    }
-};
+    const filterValue = document.getElementById('filterInput').value.toLowerCase();
+    filteredRows = filterValue ? tableRows.filter(row => {
+        return Array.from(row.cells).some(cell => {
+            return cell.textContent.toLowerCase().includes(filterValue);
+        });
+    }) : tableRows;
     
-document.getElementById('scrollToTop').onclick = function() {
-    window.scrollTo({top: 0, behavior: 'smooth'});
-};
-
-function showKeyboardNavigationMessage() {
-    const messageElement = document.getElementById('keyboard-navigation-message');
-    messageElement.style.display = 'block'; 
+    currentPage = 1; 
+    updateTableAndPagination(filteredRows);
 }
 
-function isDesktop() {
-    return navigator.userAgent.indexOf('Mobi') === -1; 
+function updateTableAndPagination(rows) {
+    showPage(currentPage, rows);
+    updatePagination(rows);
 }
-
-window.onload = function() {
-    if (isDesktop()) {
-        showKeyboardNavigationMessage(); 
-    }
-};
 
 prevSetBtn.onclick = () => {
     currentSet--;
-    updatePageNumbers();
-    showPage(currentPage);
-    updateNavigationButtons();
+    updateTableAndPagination(filteredRows);
 };
 
 nextSetBtn.onclick = () => {
     currentSet++;
-    updatePageNumbers();
-    showPage(currentPage);
-    updateNavigationButtons();
+    updateTableAndPagination(filteredRows);
 };
 
-function handleKeydown(event) {
-    if (event.key === "ArrowLeft") {
-        if (currentPage > 1) {
-            currentPage--;
-            showPage(currentPage);
-        }
-    } else if (event.key === "ArrowRight") {
-        if (currentPage < totalPages) {
-            currentPage++;
-            showPage(currentPage);
-        }
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'ArrowRight' && currentPage < Math.ceil(filteredRows.length / rowsPerPage)) {
+        currentPage++;
+    } else if (event.key === 'ArrowLeft' && currentPage > 1) {
+        currentPage--;
+    } else if (event.key === 'PageDown' && currentPage < Math.ceil(filteredRows.length / rowsPerPage)) {
+        currentPage++;
+    } else if (event.key === 'PageUp' && currentPage > 1) {
+        currentPage--;
     }
+    showPage(currentPage, filteredRows);
+    updatePagination(filteredRows);
+});
+
+
+function convertToDate(dateStr) {
+    const parts = dateStr.split('/'); 
+    return new Date(parts[2], parts[1] - 1, parts[0]); 
 }
-window.addEventListener('keydown', handleKeydown);
+
+let isAsc = false; 
+const prescricaoHeader = document.getElementById('prescricao-header');
+const sortIcon = document.getElementById('sort-icon');
+
+
+prescricaoHeader.onclick = function() {
+    filteredRows.sort((a, b) => {
+        const dateA = convertToDate(a.cells[4].textContent.trim()); 
+        const dateB = convertToDate(b.cells[4].textContent.trim());
+        
+        return isAsc ? dateA - dateB : dateB - dateA; 
+    });
+
+    isAsc = !isAsc; 
+    sortIcon.className = isAsc ? 'fa-solid fa-caret-up' : 'fa-solid fa-caret-down';
+    sortIcon.style.display = 'inline';
+
+    currentPage = 1; 
+    updatePagination(filteredRows); 
+};
+
+
+
+
+
+
+
+function convertToDateTime(dateStr) {
+    const [datePart, timePart] = dateStr.split(' ');
+    const parts = datePart.split('/'); 
+    const timeParts = timePart.split(':'); 
+    return new Date(parts[2], parts[1] - 1, parts[0], timeParts[0], timeParts[1]);
+}
+
+const admissaoHeader = document.getElementById('admissao-header');
+const sortAdmissaoIcon = document.getElementById('sort-admissao-icon');
+let isAdmissaoAsc = false; 
+admissaoHeader.onclick = function() {
+    filteredRows.sort((a, b) => {
+        const dateTimeA = convertToDateTime(a.cells[6].textContent.trim()); 
+        const dateTimeB = convertToDateTime(b.cells[6].textContent.trim()); 
+        const ageInDaysA = Math.floor((new Date() - dateTimeA) / (1000 * 60 * 60 * 24));
+        const ageInDaysB = Math.floor((new Date() - dateTimeB) / (1000 * 60 * 60 * 24));
+        if (ageInDaysA < 365 && ageInDaysB < 365) {
+            return isAdmissaoAsc ? ageInDaysA - ageInDaysB : ageInDaysB - ageInDaysA;
+        } else if (ageInDaysA < 365) {
+            return isAdmissaoAsc ? -1 : 1;
+        } else if (ageInDaysB < 365) {
+            return isAdmissaoAsc ? 1 : -1;
+        }
+        return isAdmissaoAsc ? (ageInDaysA - ageInDaysB) : (ageInDaysB - ageInDaysA);
+    });
+
+    isAdmissaoAsc = !isAdmissaoAsc; 
+    sortAdmissaoIcon.className = isAdmissaoAsc ? 'fa-solid fa-caret-up sort-icon rotate-up' : 'fa-solid fa-caret-down sort-icon rotate-down';
+    sortAdmissaoIcon.style.display = 'inline';
+
+    currentPage = 1; 
+    updatePagination(filteredRows); 
+};
+
+
+
+
+
+
+// ordenar a idade
+
+
+let isConvenioAsc = false; 
+
+
+document.getElementById('convenio-header').onclick = function() {
+    filteredRows.sort((a, b) => {
+        const convenioA = a.cells[2].textContent.trim().toLowerCase(); 
+        const convenioB = b.cells[2].textContent.trim().toLowerCase(); 
+
+        if (convenioA < convenioB) return isConvenioAsc ? -1 : 1;
+        if (convenioA > convenioB) return isConvenioAsc ? 1 : -1;
+        return 0;
+    });
+
+    isConvenioAsc = !isConvenioAsc; 
+    const sortConvenioIcon = document.getElementById('sort-convenio-icon');
+    sortConvenioIcon.className = isConvenioAsc ? 'fa-solid fa-caret-up sort-icon rotate-up' : 'fa-solid fa-caret-down sort-icon rotate-down';
+    sortConvenioIcon.style.display = 'inline';
+
+    currentPage = 1; 
+    updatePagination(filteredRows); 
+};
+let isIdadeAsc = false; 
+document.getElementById('idade-header').onclick = function() {
+    filteredRows.sort((a, b) => {
+        const idadeA = parseInt(a.cells[7].textContent.trim(), 10); 
+        const idadeB = parseInt(b.cells[7].textContent.trim(), 10); 
+
+        return isIdadeAsc ? idadeA - idadeB : idadeB - idadeA; 
+    });
+
+    isIdadeAsc = !isIdadeAsc; 
+
+    const sortIdadeIcon = document.getElementById('sort-idade-icon');
+    sortIdadeIcon.className = isIdadeAsc ? 'fa-solid fa-caret-up sort-icon rotate-up' : 'fa-solid fa-caret-down sort-icon rotate-down';
+    sortIdadeIcon.style.display = 'inline';
+
+    currentPage = 1; 
+    updatePagination(filteredRows); 
+};
+let isPacienteAsc = false; 
+document.getElementById('paciente-header').onclick = function() {
+    filteredRows.sort((a, b) => {
+        const pacienteA = a.cells[1].textContent.trim().toLowerCase(); 
+        const pacienteB = b.cells[1].textContent.trim().toLowerCase(); 
+
+        if (pacienteA < pacienteB) return isPacienteAsc ? -1 : 1;
+        if (pacienteA > pacienteB) return isPacienteAsc ? 1 : -1;
+        return 0;
+    });
+
+    isPacienteAsc = !isPacienteAsc; 
+
+    const sortPacienteIcon = document.getElementById('sort-paciente-icon');
+    sortPacienteIcon.className = isPacienteAsc ? 'fa-solid fa-caret-up sort-icon rotate-up' : 'fa-solid fa-caret-down sort-icon rotate-down';
+    sortPacienteIcon.style.display = 'inline';
+
+    currentPage = 1; 
+    updatePagination(filteredRows); 
+};
+let isHorasAsc = false; 
+
+document.getElementById('horas-header').onclick = function() {
+    filteredRows.sort((a, b) => {
+        const horasA = a.cells[7].textContent.trim(); 
+        const horasB = b.cells[7].textContent.trim(); 
+        const [hoursA, minutesA] = horasA.split(':').map(Number);
+        const [hoursB, minutesB] = horasB.split(':').map(Number);
+        
+        const totalMinutesA = hoursA * 60 + minutesA;
+        const totalMinutesB = hoursB * 60 + minutesB;
+
+        return isHorasAsc ? totalMinutesA - totalMinutesB : totalMinutesB - totalMinutesA; 
+    });
+
+    isHorasAsc = !isHorasAsc; 
+    const sortHorasIcon = document.getElementById('sort-horas-icon');
+    sortHorasIcon.className = isHorasAsc ? 'fa-solid fa-caret-up sort-icon rotate-up' : 'fa-solid fa-caret-down sort-icon rotate-down';
+    sortHorasIcon.style.display = 'inline';
+
+    currentPage = 1; 
+    updatePagination(filteredRows); 
+};
