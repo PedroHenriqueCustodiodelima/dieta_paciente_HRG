50c55bb3d2eb3eea322779e19ccfb71076834449
PedroHenriqueCustodiodeLima <ph4261009@gmail.com>
2024-10-23 10:59:10 -0300

    trocando os titulos das paginas

--- teste.php
+++ teste.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tabela de Pacientes com Paginação Dinâmica</title>
+    <title>DIETA PACIENTES</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/teste.css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap" rel="stylesheet">
